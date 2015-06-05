8ef8ac13016f8263220353b748b090890f756d8e
Nikos Mavrogiannopoulos <nmav@gnutls.org>
2015-06-05 23:50:52 +0200

    doc update

--- manual/search/all_3.js
+++ manual/search/all_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['miscellaneous_20api',['Miscellaneous API',['../group__misc-api.html',1,'']]],
-  ['main_20api_20functions',['Main API Functions',['../group__radcli-api.html',1,'']]]
+  ['main_20api',['Main API',['../group__radcli-api.html',1,'']]]
 ];
